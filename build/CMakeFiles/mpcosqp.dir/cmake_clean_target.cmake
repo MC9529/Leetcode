10c8e7a5782245f32b3e57d89fd2a474ba2685fc
MC9529 <mc9529@163.com>
2019-10-09 19:17:06 +0800

    the osqp demo

---
+++ build/CMakeFiles/mpcosqp.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libmpcosqp.a"
+)
---
+++ build/CMakeFiles/osqp.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/auxil.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/auxil.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/cs.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/cs.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/ctrlc.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/ctrlc.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/kkt.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/kkt.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/lib_handler.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/lib_handler.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/lin_alg.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/lin_alg.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/lin_sys.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/lin_sys.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/osqp.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/osqp.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/pardiso/pardiso_interface.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/pardiso/pardiso_interface.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/pardiso/pardiso_loader.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/pardiso/pardiso_loader.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/polish.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/polish.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/proj.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/proj.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/amd/src/SuiteSparse_config.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/amd/src/SuiteSparse_config.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/amd/src/amd_1.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/amd/src/amd_1.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/amd/src/amd_2.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/amd/src/amd_2.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/amd/src/amd_aat.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/amd/src/amd_aat.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/amd/src/amd_control.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/amd/src/amd_control.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/amd/src/amd_defaults.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/amd/src/amd_defaults.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/amd/src/amd_info.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/amd/src/amd_info.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/amd/src/amd_order.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/amd/src/amd_order.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/amd/src/amd_post_tree.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/amd/src/amd_post_tree.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/amd/src/amd_postorder.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/amd/src/amd_postorder.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/amd/src/amd_preprocess.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/amd/src/amd_preprocess.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/amd/src/amd_valid.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/amd/src/amd_valid.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/qdldl.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/qdldl.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/qdldl/qdldl_interface.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/qdldl/qdldl_interface.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/scaling.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/scaling.c.o"
+  "/home/ccg/File/osqp/hello_for_osqpbackup/osqp/src/util.c" "/home/ccg/File/osqp/hello_for_osqpbackup/build/CMakeFiles/osqp.dir/osqp/src/util.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "../include"
+  "../osqp/include"
+  "../osqp/include/qdldl"
+  "../osqp/include/qdldl/include"
+  "../osqp/include/pardiso"
+  "../mpc_osqp"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
